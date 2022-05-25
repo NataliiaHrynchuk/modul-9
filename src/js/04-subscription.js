7f818f228364c746010a3bd51305f711ea1582db
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-05-25 15:30:19 +0300

    update

--- src/js/04-subscription.js
+++ src/js/04-subscription.js
@@ -1,11 +1,6 @@
 
 import '../css/common.css';
 
-const refs = {
-  modal: document.querySelector('#subscription-modal'),
-  subscribeBtn: document.querySelector('button[data-subscribe]'),
-};
-
 /**
  * ====Приклад без використання Бутстрапу===
  */
@@ -35,26 +30,48 @@ const refs = {
  */
 import BSN from 'bootstrap.native';
 
+const refs = {
+  modal: document.querySelector('#subscription-modal'),
+  subscribeBtn: document.querySelector('button[data-subscribe]'),
+};
+
 const modal = new BSN.Modal('#subscription-modal');
 console.log(modal);
-modal.show();
-// openModal();
+// modal.show();//Модальне вікно відкриється одразу при завантаженні сторінки
+
+const PROMPT_DELAY = 1000;//Проміжок часу, через який буде викликатись ф-ція
+const MAX_PROMPT_ATTEMPTS = 3;//К-сть разів, скільки буде викликатись ф-ція
+let promptCounter = 0;
+
+// setTimeout(() => {
+//   console.log('Відкриваємо модалку');
+// modal.show();
+// }, PROMPT_DELAY)// Модальне вікно відкриється через PROMPT_DELAY мс, має закриватись 
+//при натисканні на кнопку close
+openModal();
+
+//-- Вішаємо слухача на весь основний div з id="subscription-modal". При кліку на кнопку close відбувається подія
+//--hide.bs.modal. Її використаємо у якості івента і запустимо setTimeout кожного разу, як закривається модалка
+
+refs.modal.addEventListener('hide.bs.modal', () => {
+  openModal();
+});
 
 // refs.modal.addEventListener('hide.bs.modal', openModal);
 // refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
 
-// function openModal() {
+function openModal() {
 //   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
 //     console.log('Максимальное кол-во надоеданий или подписался');
 //     return;
 //   }
 
-//   setTimeout(() => {
-//     console.log('Открываем надоедалку');
-//     modal.show();
-//     promptCounter += 1;
-//   }, PROMPT_DELAY);
-// }
+  setTimeout(() => {
+    console.log('Открываем модалку');
+    modal.show();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
+};
 
 // function onSubscribeBtnClick() {
 //   hasSubscribed = true;
