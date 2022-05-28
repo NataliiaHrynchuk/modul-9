3ea081604cd564ca07ad968592e90bbea6c95212
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-28 15:51:44 +0300

    update

--- src/js/Task-2.js
+++ src/js/Task-2.js
@@ -11,17 +11,17 @@ const refs = {
     dataSeconds: document.querySelector('span[data-seconds]'),
 }
 
-console.log(refs.input);
-console.log(refs.startBtn);
-console.log(refs.dataDays);
-console.log(refs.dataHours);
-console.log(refs.dataMinutes);
-console.log(refs.dataSeconds);
-let selectedDate = null;
-
-refs.input.addEventListener('change', () => {
-    
-});
+// console.log(refs.startBtn);
+// console.log(refs.dataDays);
+// console.log(refs.dataHours);
+// console.log(refs.dataMinutes);
+// console.log(refs.dataSeconds);
+let selectedTime = null;
+let currentTime = new Date();
+ refs.startBtn.setAttribute("disabled", true);
+ refs.startBtn.addEventListener('click', () => {
+    timer.start();
+  });
 
 // function flatpickr(selector, options);
 const options = {
@@ -32,15 +32,83 @@ const options = {
     minuteIncrement: 1,
         onClose(selectedDates) {
         //    console.log(selectedDates[0]);
-           selectedDate = selectedDates[0];
-           console.log(selectedDate);
+           selectedTime = selectedDates[0];
+
+           if(selectedTime < currentTime) {
+             window.alert("Please choose a date in the future");
+             return;
+           }
+           refs.startBtn.removeAttribute("disabled");
+          
+          //  console.log(selectedDate.getTime());
       
       
     },
   };
-  console.log(options.defaultDate);
-  console.log(selectedDate);
- 
-//   console.log(selectedDate);
-//   console.log(refs.input.value);
+
+  const timer = {
+    intervalId: null,
+    isActive: false,
+    start() {
+      if (this.isActive) {
+        return;
+      }
+      // const currentTime = Date.now();
+      refs.input.setAttribute('disabled', true);
+      this.isActive = true;
+
+      this.intervalId = setInterval(() => {
+        const currentTime = new Date();
+        // console.log('currentTime', currentTime);
+        // console.log("selectedTime", selectedTime);
+        const deltaTime = selectedTime - currentTime;
+        const {days, hours, minutes, seconds} = convertMs(deltaTime);
+      if(deltaTime < 0) {
+        
+        this.stop();
+        return;
+      }
+      
+      // console.log(deltaTime);
+      console.log(`${days}, ${hours}, ${minutes}, ${seconds}`);
+      refs.dataDays.textContent = `${days}`;
+      refs.dataHours.textContent = `${hours}`;
+      refs.dataMinutes.textContent = `${minutes}`;
+      refs.dataSeconds.textContent = `${seconds}`;
+
+      }, 1000);
+
+    },
+    stop() {
+      clearInterval(this.intervalId);
+    this.isActiv = false;
+    }
+
+  }
+  
+  
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  };
+
+ function pad(value) {
+      return String(value).padStart(2, '0');
+  };
+
 flatpickr("#datetime-picker", options);
+// console.log(convertMs(543544455454354));
